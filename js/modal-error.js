d5bba1b940bc094651edc1983d5705b24f1c4808
Alex Kachar <alexkachar.github@gmail.com>
2018-08-11 14:19:41 +0300

    modules 1

---
+++ js/modal-error.js
@@ -0,0 +1,5 @@
+const templateError =
+`<section class="modal">
+  <h2 class="modal__title">Произошла ошибка!</h2>
+  <p class="modal__text">Статус: 404. Пожалуйста, перезагрузите страницу.</p>
+</section>`;
---
+++ js/utils.js
@@ -0,0 +1,14 @@
+const mainElement = document.querySelector(`.main`);
+
+const getElementFromTemplate = (template) => {
+  const wrapper = document.createElement(`div`);
+  wrapper.innerHTML = template;
+  return wrapper.children[0];
+};
+
+const renderScreen = (gameScreen) => {
+  mainElement.innerHTML = ``;
+  mainElement.appendChild(gameScreen);
+};
+
+export {getElementFromTemplate, renderScreen};
