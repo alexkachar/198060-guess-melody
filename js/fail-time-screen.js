e7c4080b677653251aa5247763d784b1dafb334f
Alex Kachar <alexkachar.github@gmail.com>
2018-08-12 21:04:57 +0300

    fail screens modules

---
+++ js/fail-time-screen.js
@@ -0,0 +1,17 @@
+import {getElementFromTemplate, renderScreen} from './utils';
+import {welcomeScreenElement} from './welcome-screen';
+
+const templateFailTime =
+`<section class="result">
+  <div class="result__logo"><img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83"></div>
+  <h2 class="result__title">Увы и ах!</h2>
+  <p class="result__total result__total--fail">Время вышло! Вы не успели отгадать все мелодии</p>
+  <button class="result__replay" type="button">Попробовать ещё раз</button>
+</section>`;
+
+const failTimeElement = getElementFromTemplate(templateFailTime);
+
+const replayButton = failTimeElement.querySelector(`.result__replay`);
+replayButton.addEventListener(`click`, () => renderScreen(welcomeScreenElement));
+
+export {failTimeElement};
---
+++ js/fail-tries-screen.js
@@ -0,0 +1,17 @@
+import {getElementFromTemplate, renderScreen} from './utils';
+import {welcomeScreenElement} from './welcome-screen';
+
+const templateFailTries =
+`<section class="result">
+  <div class="result__logo"><img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83"></div>
+  <h2 class="result__title">Какая жалость!</h2>
+  <p class="result__total result__total--fail">У вас закончились все попытки. Ничего, повезёт в следующий раз!</p>
+  <button class="result__replay" type="button">Попробовать ещё раз</button>
+</section>`;
+
+const failTriesElement = getElementFromTemplate(templateFailTries);
+
+const replayButton = failTriesElement.querySelector(`.result__replay`);
+replayButton.addEventListener(`click`, () => renderScreen(welcomeScreenElement));
+
+export {failTriesElement};
