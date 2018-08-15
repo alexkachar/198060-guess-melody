1e56405593713572b944e937c82b4763286b3a3e
Alex Kachar <alexkachar.github@gmail.com>
2018-08-16 00:19:58 +0300

    circular dependencies fix - version 2 - router

---
+++ js/constants.js
@@ -0,0 +1,5 @@
+export const WELCOME = `WELCOME`;
+export const GENRE = `GENRE`;
+export const TIME = `TIME`;
+export const TRIES = `TRIES`;
+export const SUCCESS = `SUCCESS`;
--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+import {Routes} from './routes';
+import {WELCOME, GENRE, ARTIST, SUCCESS, TIME, TRIES} from './constants';
 import {welcomeScreenElement, welcomeButton} from './welcome-screen';
 import {renderScreen} from './utils';
 import {genreScreenElement, backButton as genreBackButton} from './genre-screen';
@@ -6,7 +8,8 @@ import {replayButton as successReplayButton} from './success-screen';
 import {replayButton as timeReplayButton} from './fail-time-screen';
 import {replayButton as triesReplayButton} from './fail-tries-screen';
 
-renderScreen(welcomeScreenElement);
+// renderScreen(welcomeScreenElement);
+renderScreen(Routes[WELCOME]);
 
 genreBackButton.addEventListener(`click`, () => renderScreen(welcomeScreenElement));
 artistBackButton.addEventListener(`click`, () => renderScreen(welcomeScreenElement));
