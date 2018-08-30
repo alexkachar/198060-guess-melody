7491758bbae6973a7d8f5d08ab04c6dae985da1a
Alex Kachar <alexkachar.github@gmail.com>
2018-08-30 21:08:49 +0300

    data levels refactoring

--- js/data/data.js
+++ js/data/data.js
@@ -10,111 +10,155 @@ const game = {
   rules: [`За 5 минут нужно ответить на все вопросы.`, `Можно допустить 3 ошибки.`]
 };
 
+const music = [{
+  artist: `Kevin MacLeod`,
+  name: `Long Stroll`,
+  image: `https://yt3.ggpht.com/-fkDeGauT7Co/AAAAAAAAAAI/AAAAAAAAAAA/dkF5ZKkrxRo/s900-c-k-no-mo-rj-c0xffffff/photo.jpg`,
+  src: `https://www.youtube.com/audiolibrary_download?vid=91624fdc22fc54ed`,
+  genre: `Jazz`
+},
+{
+  artist: `Jingle Punks`,
+  name: `In the Land of Rhinoplasty`,
+  image: `https://i.vimeocdn.com/portrait/992615_300x300`,
+  src: `https://www.youtube.com/audiolibrary_download?vid=dc3b4dc549becd6b`,
+  genre: `Rock`
+},
+{
+  artist: `Audionautix`,
+  name: `Travel Light`,
+  image: `http://4.bp.blogspot.com/-kft9qu5ET6U/VPFUBi9W-MI/AAAAAAAACYM/UxXilXKYwOc/s1600/audionautix%2BHalf%2BSize.jpg`,
+  src: `https://www.youtube.com/audiolibrary_download?vid=a127d9b7de8a17cf`,
+  genre: `Country`
+},
+{
+  artist: `Riot`,
+  name: `	Level Plane`,
+  image: `https://i.ytimg.com/vi/jzgM3m8Vp1k/maxresdefault.jpg`,
+  src: `https://www.youtube.com/audiolibrary_download?vid=dfb828f40096184c`,
+  genre: `R&B`
+},
+{
+  artist: `Jingle Punks`,
+  name: `Lucky Day`,
+  image: `https://i.vimeocdn.com/portrait/992615_300x300`,
+  src: `https://www.youtube.com/audiolibrary_download?vid=bcbe5be936a32fb1`,
+  genre: `Pop`
+},
+{
+  artist: `Quincas Moreira`,
+  name: `Firefly`,
+  image: `http://www.atribuna.com.br/fileadmin/_processed_/csm_Quincas-Moreira-Foto-Divulgacao_76d1a8b00e.jpg`,
+  src: `https://www.youtube.com/audiolibrary_download?vid=79100e44c826e2f7`,
+  genre: `Electronic`
+}
+];
+
 const levels = {
   '1': {
-    audio: `https://www.youtube.com/audiolibrary_download?vid=91624fdc22fc54ed`,
+    audio: music[0].src,
     type: `artist`,
     answers: [
       {
         value: `1`,
-        img: `https://yt3.ggpht.com/-fkDeGauT7Co/AAAAAAAAAAI/AAAAAAAAAAA/dkF5ZKkrxRo/s900-c-k-no-mo-rj-c0xffffff/photo.jpg`,
-        artist: `Kevin MacLeod`,
+        img: music[0].image,
+        artist: music[0].artist,
         correct: true
       },
       {
         value: `2`,
-        img: `http://4.bp.blogspot.com/-kft9qu5ET6U/VPFUBi9W-MI/AAAAAAAACYM/UxXilXKYwOc/s1600/audionautix%2BHalf%2BSize.jpg`,
-        artist: `Audionautix`,
+        img: music[2].image,
+        artist: music[2].artist,
         correct: false
       },
       {
         value: `3`,
-        img: `https://i.ytimg.com/vi/jzgM3m8Vp1k/maxresdefault.jpg`,
-        artist: `Riot`,
+        img: music[3].image,
+        artist: music[3].artist,
         correct: false
       }
     ]
   },
   '2': {
     type: `artist`,
-    audio: `https://www.youtube.com/audiolibrary_download?vid=dc3b4dc549becd6b`,
+    audio: music[1].src,
     answers: [
       {
-        img: `http://4.bp.blogspot.com/-kft9qu5ET6U/VPFUBi9W-MI/AAAAAAAACYM/UxXilXKYwOc/s1600/audionautix%2BHalf%2BSize.jpg`,
-        artist: `Audionautix`,
+        img: music[2].image,
+        artist: music[2].artist,
         correct: false
       },
       {
-        img: `https://i.vimeocdn.com/portrait/992615_300x300`,
-        artist: `Jingle Punks`,
+        img: music[1].image,
+        artist: music[1].artist,
         correct: true
       },
       {
-        img: `http://www.atribuna.com.br/fileadmin/_processed_/csm_Quincas-Moreira-Foto-Divulgacao_76d1a8b00e.jpg`,
-        artist: `Quincas Moreira`,
+        img: music[5].image,
+        artist: music[5].artist,
         correct: false
       }
     ]
   },
   '3': {
     type: `artist`,
-    audio: `https://www.youtube.com/audiolibrary_download?vid=a127d9b7de8a17cf`,
+    audio: music[2].src,
     answers: [
       {
-        img: `http://4.bp.blogspot.com/-kft9qu5ET6U/VPFUBi9W-MI/AAAAAAAACYM/UxXilXKYwOc/s1600/audionautix%2BHalf%2BSize.jpg`,
-        artist: `Audionautix`,
+        img: music[2].image,
+        artist: music[2].artist,
         correct: true
       },
       {
-        img: `https://i.ytimg.com/vi/jzgM3m8Vp1k/maxresdefault.jpg`,
-        artist: `Riot`,
+        img: music[3].image,
+        artist: music[3].artist,
         correct: false
       },
       {
-        img: `https://yt3.ggpht.com/-fkDeGauT7Co/AAAAAAAAAAI/AAAAAAAAAAA/dkF5ZKkrxRo/s900-c-k-no-mo-rj-c0xffffff/photo.jpg`,
-        artist: `Kevin MacLeod`,
+        img: music[0].image,
+        artist: music[0].artist,
         correct: false
       }
     ]
   },
   '4': {
     type: `artist`,
-    audio: `https://www.youtube.com/audiolibrary_download?vid=dfb828f40096184c`,
+    audio: music[3].src,
     answers: [
       {
-        img: `https://i.vimeocdn.com/portrait/992615_300x300`,
+        img: music[1].image,
         artist: `Jingle Punks`,
         correct: false
       },
       {
-        img: `http://4.bp.blogspot.com/-kft9qu5ET6U/VPFUBi9W-MI/AAAAAAAACYM/UxXilXKYwOc/s1600/audionautix%2BHalf%2BSize.jpg`,
-        artist: `Audionautix`,
+        img: music[2].image,
+        artist: music[2].artist,
         correct: false
       },
       {
-        img: `https://i.ytimg.com/vi/jzgM3m8Vp1k/maxresdefault.jpg`,
-        artist: `Riot`,
+        img: music[3].image,
+        artist: music[3].artist,
         correct: true
       }
     ]
   },
   '5': {
     type: `artist`,
-    audio: `https://www.youtube.com/audiolibrary_download?vid=bcbe5be936a32fb1`,
+    audio: music[4].src,
     answers: [
       {
-        img: `http://www.atribuna.com.br/fileadmin/_processed_/csm_Quincas-Moreira-Foto-Divulgacao_76d1a8b00e.jpg`,
-        artist: `Quincas Moreira`,
+        img: music[5].image,
+        artist: music[5].artist,
         correct: false
       },
       {
-        img: `https://i.vimeocdn.com/portrait/992615_300x300`,
-        artist: `Jingle Punks`,
+        img: music[1].image,
+        artist: music[1].artist,
         correct: true
       },
       {
-        img: `https://yt3.ggpht.com/-fkDeGauT7Co/AAAAAAAAAAI/AAAAAAAAAAA/dkF5ZKkrxRo/s900-c-k-no-mo-rj-c0xffffff/photo.jpg`,
-        artist: `Kevin MacLeod`,
+        img: music[0].image,
+        artist: music[0].artist,
         correct: false
       }
     ]
@@ -124,26 +168,26 @@ const levels = {
     genre: `pop`,
     answers: [
       {
-        audio: `https://www.youtube.com/audiolibrary_download?vid=bcbe5be936a32fb1`,
-        genre: `pop`,
+        audio: music[4].src,
+        genre: music[4].src,
         correct: true,
         autoplay: true
       },
       {
-        audio: `https://www.youtube.com/audiolibrary_download?vid=a127d9b7de8a17cf`,
-        genre: `Country`,
+        audio: music[2].src,
+        genre: music[2].genre,
         correct: false,
         autoplay: false
       },
       {
-        audio: `https://www.youtube.com/audiolibrary_download?vid=a127d9b7de8a17cf`,
-        genre: `Country`,
+        audio: music[2].src,
+        genre: music[2].genre,
         correct: false,
         autoplay: false
       },
       {
-        audio: `https://www.youtube.com/audiolibrary_download?vid=bcbe5be936a32fb1`,
-        genre: `pop`,
+        audio: music[4].src,
+        genre: music[4].genre,
         correct: true,
         autoplay: false
       },
@@ -154,26 +198,26 @@ const levels = {
     genre: `Jazz`,
     answers: [
       {
-        audio: `https://www.youtube.com/audiolibrary_download?vid=a127d9b7de8a17cf`,
-        genre: `Country`,
+        audio: music[2].src,
+        genre: music[2].genre,
         correct: false,
         autoplay: true
       },
       {
-        audio: `https://www.youtube.com/audiolibrary_download?vid=dc3b4dc549becd6b`,
-        genre: `Rock`,
+        audio: music[1].src,
+        genre: music[1].genre,
         correct: false,
         autoplay: false
       },
       {
-        audio: `https://www.youtube.com/audiolibrary_download?vid=91624fdc22fc54ed`,
-        genre: `Jazz`,
+        audio: music[0].src,
+        genre: music[0].genre,
         correct: true,
         autoplay: false
       },
       {
-        audio: `http://www.atribuna.com.br/fileadmin/_processed_/csm_Quincas-Moreira-Foto-Divulgacao_76d1a8b00e.jpg`,
-        genre: `Electronic`,
+        audio: music[5].src,
+        genre: music[5].genre,
         correct: false,
         autoplay: false
       }
@@ -184,26 +228,26 @@ const levels = {
     type: `genre`,
     answers: [
       {
-        audio: `https://www.youtube.com/audiolibrary_download?vid=a127d9b7de8a17cf`,
-        genre: `Country`,
+        audio: music[2].src,
+        genre: music[2].genre,
         correct: false,
         autoplay: true
       },
       {
-        audio: `https://www.youtube.com/audiolibrary_download?vid=dc3b4dc549becd6b`,
-        genre: `Rock`,
+        audio: music[1].src,
+        genre: music[1].genre,
         correct: true,
         autoplay: false
       },
       {
-        audio: `https://www.youtube.com/audiolibrary_download?vid=91624fdc22fc54ed`,
-        genre: `Jazz`,
+        audio: music[0].src,
+        genre: music[0].genre,
         correct: false,
         autoplay: false
       },
       {
-        audio: `https://www.youtube.com/audiolibrary_download?vid=dc3b4dc549becd6b`,
-        genre: `Rock`,
+        audio: music[1].src,
+        genre: music[1].genre,
         correct: true,
         autoplay: false
       },
@@ -214,26 +258,26 @@ const levels = {
     type: `genre`,
     answers: [
       {
-        audio: `https://www.youtube.com/audiolibrary_download?vid=a127d9b7de8a17cf`,
-        genre: `Country`,
+        audio: music[2].src,
+        genre: music[2].genre,
         correct: true,
         autoplay: true
       },
       {
-        audio: `https://www.youtube.com/audiolibrary_download?vid=79100e44c826e2f7`,
-        genre: `Electronic`,
+        audio: music[5].src,
+        genre: music[5].genre,
         correct: false,
         autoplay: false
       },
       {
-        audio: `https://www.youtube.com/audiolibrary_download?vid=a127d9b7de8a17cf`,
-        genre: `Country`,
+        audio: music[2].src,
+        genre: music[2].genre,
         correct: true,
         autoplay: false
       },
       {
-        audio: `https://www.youtube.com/audiolibrary_download?vid=bcbe5be936a32fb1`,
-        genre: `pop`,
+        audio: music[4].src,
+        genre: music[4].genre,
         correct: false,
         autoplay: false
       }
@@ -244,26 +288,26 @@ const levels = {
     type: `genre`,
     answers: [
       {
-        audio: `https://www.youtube.com/audiolibrary_download?vid=bcbe5be936a32fb1`,
-        genre: `pop`,
+        audio: music[4].src,
+        genre: music[4].genre,
         correct: false,
         autoplay: true
       },
       {
-        audio: `https://www.youtube.com/audiolibrary_download?vid=bcbe5be936a32fb1`,
-        genre: `pop`,
+        audio: music[4].src,
+        genre: music[4].genre,
         correct: false,
         autoplay: false
       },
       {
-        audio: `http://www.atribuna.com.br/fileadmin/_processed_/csm_Quincas-Moreira-Foto-Divulgacao_76d1a8b00e.jpg`,
-        genre: `Electronic`,
+        audio: music[5].image,
+        genre: music[5].genre,
         correct: true,
         autoplay: false
       },
       {
-        audio: `http://www.atribuna.com.br/fileadmin/_processed_/csm_Quincas-Moreira-Foto-Divulgacao_76d1a8b00e.jpg`,
-        genre: `Electronic`,
+        audio: music[5].image,
+        genre: music[5].genre,
         correct: true,
         autoplay: false
       }
